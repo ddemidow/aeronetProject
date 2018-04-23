3bd87f2988b6ae4c32d11c1298da5f0069814085
Dima Demidow <dzmitry.dzemidau@itechart-group.com>
2018-04-23 17:26:24 +0300

    Discovery Item DAO

--- src/main/java/com/example/dao/mongodb/DiscoveryItemMongoDAO.java
+++ src/main/java/com/example/dao/mongodb/DiscoveryItemMongoDAO.java
@@ -3,32 +3,77 @@ package com.example.dao.mongodb;
 import com.example.dao.DiscoveryItemDAO;
 import com.example.entities.DiscoveryItem;
 import com.example.entities.Location;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DBObject;
+import com.mongodb.*;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class DiscoveryItemMongoDAO implements DiscoveryItemDAO {
+    private DBCollection discoveryItemCollection;
 
     @Override
-    public ArrayList<DiscoveryItem> getAllItemsFromLocation() {
-        return null;
+    public ArrayList<DiscoveryItem> getAllItemsFromLocation(String locationName) {
+        discoveryItemCollection = new MongoClient().getDB("test").getCollection(locationName);
+
+        ArrayList<DiscoveryItem> discoveryItems = new ArrayList<DiscoveryItem>();
+
+        DBObject discoveryItemLooksLikeModel = new BasicDBObject("AERONET_Site", locationName);
+        DBCursor cursor = discoveryItemCollection.find(discoveryItemLooksLikeModel);
+
+        while (cursor.hasNext()) {
+            discoveryItems.add(toJavaObject(cursor.next()));
+        }
+
+        return discoveryItems;
     }
 
     @Override
-    public ArrayList<DiscoveryItem> getAllItemsBetween(Date strartFrom, Date endWith) {
-        return null;
+    public ArrayList<DiscoveryItem> getAllItemsBetween(LocalDate startFrom, LocalDate endWith, String locationName) {
+        discoveryItemCollection = new MongoClient().getDB("test").getCollection(locationName);
+
+        ArrayList<DiscoveryItem> discoveryItems = new ArrayList<DiscoveryItem>();
+        DBObject discoveryItemLooksLikeModel = new BasicDBObject("AERONET_Site", locationName);
+        discoveryItemLooksLikeModel.put("Date(dd:mm:yyyy)", BasicDBObjectBuilder.start("$gte", startFrom).add("$lte", endWith).get());
+
+        DBCursor cursor = discoveryItemCollection.find(discoveryItemLooksLikeModel);
+
+        while (cursor.hasNext()) {
+            discoveryItems.add(toJavaObject(cursor.next()));
+        }
+
+        return discoveryItems;
     }
 
     @Override
-    public Boolean insertItem(DiscoveryItem newItem, Location parentLocation) {
-        return null;
+    public Boolean insertItem(DiscoveryItem newItem, String parentLocation) {
+        discoveryItemCollection = new MongoClient().getDB("test").getCollection(parentLocation);
+
+        try {
+            discoveryItemCollection.insert(toDbObject(newItem));
+
+            return true;
+        } catch(Exception ex) {
+            return false;
+        }
     }
 
     @Override
-    public Boolean insertItems(ArrayList<DiscoveryItem> newItems, Location parentLocation) {
-        return null;
+    public Boolean insertItems(ArrayList<DiscoveryItem> newItems, String parentLocation) {
+        discoveryItemCollection = new MongoClient().getDB("test").getCollection(parentLocation);
+        ArrayList<DBObject> convertedDiscoveryItems = new ArrayList<>();
+
+        for (DiscoveryItem currentDiscoveryItem : newItems) {
+            convertedDiscoveryItems.add(toDbObject(currentDiscoveryItem));
+        }
+
+        try {
+            discoveryItemCollection.insert(convertedDiscoveryItems);
+
+            return true;
+        } catch(Exception ex) {
+            return false;
+        }
     }
 
     public DBObject toDbObject(DiscoveryItem item) {
@@ -42,6 +87,12 @@ public class DiscoveryItemMongoDAO implements DiscoveryItemDAO {
     }
 
     public DiscoveryItem toJavaObject(DBObject dbDiscoveryItem) {
-        return null;
+        DiscoveryItem discoveryItem = new DiscoveryItem();
+
+        for (String currentFieldName : dbDiscoveryItem.keySet()) {
+            discoveryItem.getFieldToValue().put(currentFieldName, dbDiscoveryItem.get(currentFieldName));
+        }
+
+        return discoveryItem;
     }
 }
